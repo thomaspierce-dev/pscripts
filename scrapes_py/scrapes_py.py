5ec295955cd24d74323a064960f1b344c12e878e
tpierce-dev <thecodegetter@gmail.com>
2019-11-10 22:27:11 -0500

    Updates

--- scrapes_py/scrapes_py.py
+++ scrapes_py/scrapes_py.py
@@ -6,5 +6,23 @@ from bs4 import BeautifulSoup
 # Beautiful allows up to grab the HTML data and clean it up. Converts string(s)
 # into an object that can be used and manipulated.
 
-response = requests.get('https://news.ycombinator.com/news')
+url = 'https://news.ycombinator.com/news' # hn or Hacker News site
+response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser') # parse the returned HTML
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText() # Gets text inside of HTML/XML tags
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', '')) # convert to int; replace string with empty str
+            print(points)
+            hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+print(create_custom_hn(links, subtext))
